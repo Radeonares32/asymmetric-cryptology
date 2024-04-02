83ef8ee9e4a5c9a8f2f1dedc5ccf17dd7280a42c
Radeonares32 <bugra.linux.js@gmail.com>
2024-04-02 11:13:52 +0300

    encryptField word len bug fixed

--- src/main.ts
+++ src/main.ts
@@ -1,37 +1,39 @@
-import { AES,enc } from 'crypto-js'
+import { AES, enc } from "crypto-js";
 
 export class AsymmetricEncryption {
-    private key;
-    constructor(key:string) {
-      this.key = key;
-    }
-    encrypt = async (data: any[] | any): Promise<any> => {
-      if (Array.isArray(data)) {
-        return await Promise.all(data.map((value: any) => this.encrypt(value)));
-      }
-      const fieldToEncrypt = Object.keys(data);
-      const encryptData: any = {};
-      for (const field of fieldToEncrypt) {
-        encryptData[field] = this.encryptField(data[field]);
-      }
-      return encryptData;
-    };
-    decrypt = async (data: any[] | any): Promise<any> => {
-      if (Array.isArray(data)) {
-        return await Promise.all(data.map((value: any) => this.decrypt(value)));
-      }
-      const fieldToDecrypt = Object.keys(data);
-      const decryptData: any = {};
-      for (const field of fieldToDecrypt) {
-        decryptData[field] = this.decryptField(data[field]);
-      }
-      return decryptData;
-    };
-    private encryptField = (data: any) => {
-      return AES.encrypt(data, this.key).toString();
-    };
-    private decryptField = (data: any) => {
-      return AES.decrypt(data, this.key).toString(enc.Utf8);
-    };
+  private key;
+  constructor(key: string) {
+    this.key = key;
   }
-  
+  encrypt = async (data: any[] | any): Promise<any> => {
+    if (Array.isArray(data)) {
+      return await Promise.all(data.map((value: any) => this.encrypt(value)));
+    }
+    const fieldToEncrypt = Object.keys(data);
+    const encryptData: any = {};
+    for (const field of fieldToEncrypt) {
+      console.log(data[field])
+      encryptData[field] = this.encryptField(data[field]);
+    }
+    return encryptData;
+  };
+  decrypt = async (data: any[] | any): Promise<any> => {
+    if (Array.isArray(data)) {
+      return await Promise.all(data.map((value: any) => this.decrypt(value)));
+    }
+    const fieldToDecrypt = Object.keys(data);
+    const decryptData: any = {};
+    for (const field of fieldToDecrypt) {
+      decryptData[field] = this.decryptField(data[field]);
+    }
+    return decryptData;
+  };
+  private encryptField = (data: any) => {
+    console.log(data)
+    return AES.encrypt(data.toString(), this.key).toString();
+  };
+  private decryptField = (data: any) => {
+    return AES.decrypt(data.toString(), this.key).toString(enc.Utf8);
+  };
+}
+
